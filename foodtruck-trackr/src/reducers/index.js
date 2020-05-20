833a2538e3666030d3000687d4f31463530033fc
unknown <tommyconner96@gmail.com>
2020-05-20 10:26:01 -0400

    CRA done and Redux installed

---
+++ foodtruck-trackr/src/reducers/index.js
@@ -0,0 +1,20 @@
+// import {
+// ACTIONS_GOES_HERE
+//   } from '../actions'
+
+  const initialState = {
+    food: '',
+  }
+
+  export const Reducer = (state = initialState, action) => {
+    switch (action.type) {
+    //   case CASE_GOES_HERE: 
+    //     return {
+
+    //     }
+
+      default:
+        return state
+    }
+  }
+  
