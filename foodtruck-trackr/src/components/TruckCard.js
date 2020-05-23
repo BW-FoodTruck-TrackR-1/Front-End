80ac08414d97ba9e5309306b5ed14dce57856552
unknown <tommyconner96@gmail.com>
2020-05-22 21:24:41 -0400

    truckCard and menu reducer worked on

---
+++ foodtruck-trackr/src/components/TruckCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { connect } from "react-redux";
+import { getTrucks } from '../actions/TruckActions'
+import Truck from './Truck'
+
+
+const TruckCard = (props) => {
+
+    useEffect(() => {
+        props.getTrucks()
+    }, [])
+
+  return (
+    <div>
+      <div className='truck-card'>
+          {props.truck.map((truck) => {
+              return <Truck key={truck.id} truck={truck} />
+          })}
+
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+    return {
+        truck: state.truck
+    }
+  }
+
+  export default connect
+  (mapStateToProps,{getTrucks})
+  (TruckCard)
