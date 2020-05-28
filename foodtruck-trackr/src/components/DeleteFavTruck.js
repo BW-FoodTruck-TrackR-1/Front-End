45bb65c9f4d1b61025bd177f3e3ca6a779a40a5c
CWC-88 <29718763+CWC-88@users.noreply.github.com>
2020-05-28 17:23:32 -0500

    favs

--- foodtruck-trackr/src/components/DeleteFavTruck.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import axios from "axios";
-import styled from "styled-components";
-import { Route, Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import {connect} from 'react-redux';
-// import addTruck, { ADDED, DELETED } from '../actions/TruckActions';
-import * as TruckActions from '../actions/TruckActions'
